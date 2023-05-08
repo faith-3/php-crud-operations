7cc6e8024a1cc49ecff5693e1f5583078244ed13
faith-3 <122223580+faith-3@users.noreply.github.com>
2023-05-08 21:23:37 +0200

    Add files via upload

---
+++ login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Login form</title>
+</head>
+<body>
+  <h2>Login form</h2>
+  <form action="fetch.php" method="post">
+  Username: <br><input type="email" name="email" value="email"><br>
+  Password: <br><input type="password" name="password" value="password"><br>
+
+  <input type="submit" value="submit" name="submit">
+  </form>
+</body>
+</html>
